9aa9cbfcf8035ea77271bb757eb3e7f2eca756c1
gabriela tururu <gabriela.aguayaire@gmail.com>
2023-06-27 21:12:49 -0600

    WIP_ filter implementation pending to fix bug created in HOME component when calling complaintsList

--- src/components/ComplaintsList/ComplaintsList.js
+++ src/components/ComplaintsList/ComplaintsList.js
@@ -1,17 +1,47 @@
 import './ComplaintsList.css'
 import useFetch from '../Hooks/useFetch';
 import { Link } from 'react-router-dom';
+import {useState} from 'react'
 
 // const ComplaintsList = ({complaints}) => {
 const ComplaintsList = () => {
     
     const {complaints} = useFetch('http://localhost:8000/complaintsList')
+   
+    const [category, setCategory] = useState('todas')
+
+    const handleChange = () =>{
+        const selectedVal = document.querySelector('select').value
+        setCategory(selectedVal)
+    }
 
     return (      
             <div className="complaintsContainer">
                 <h1>Lista de Quejas</h1>
-                
-                {complaints && complaints.map((complaint) => (
+                <h2>La categoría actual es {category}</h2>
+                <select name="category" onChange={handleChange}>
+                    <option value="todas" defaultValue>Todas</option>
+                    <option value="aerolineas">Aerolineas</option>
+                    <option value="internet">Internet</option>
+                </select>    
+
+                {/* //el error se asocia a que en la HOME llamo a este componente y este componente 
+                exige un FILTER y el valor de filter no existe en HOME porque no hay un select */}
+
+                {complaints && category && category === 'todas'? 
+                    complaints.map((complaint) => (
+                        <div className="complaintSnippet" key={complaint.id}>
+                            <Link to={`/quejas/${complaint.id}`}>                     
+                                <h2>"{complaint.title}"</h2>
+                                <h3>Empresa: {complaint.company}</h3>
+                                <h3>Curso: {complaint.course}</h3>
+                                <p>{complaint.body.length > 200 ? `${complaint.body.substring(0,200)}...` : complaint.body}</p>   
+                            </Link>
+                        </div>
+                    ))                
+                :
+                    
+                    complaints.filter((complaint) => complaint.category === category).map((complaint) => (
                     <div className="complaintSnippet" key={complaint.id}>
                         <Link to={`/quejas/${complaint.id}`}>                     
                             <h2>"{complaint.title}"</h2>
@@ -20,8 +50,8 @@ const ComplaintsList = () => {
                             <p>{complaint.body.length > 200 ? `${complaint.body.substring(0,200)}...` : complaint.body}</p>   
                         </Link>
                     </div>
-                ))}   
-                      
+                    ))   
+                }                         
             </div>      
      );
 }
